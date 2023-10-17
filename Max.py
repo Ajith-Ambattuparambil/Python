b978e822b30d6bf37cc9cc7d38b112ee7899732f
Ajith Mani Santhosh <146512478+Ajith-Ambattuparambil@users.noreply.github.com>
2023-10-17 11:55:20 +0530

    Delete Max.py

--- Max.py
+++
@@ -1,4 +0,0 @@
-a=int(input("Enter the first Number: "))
-b=int(input("Enter the second Number: "))
-c=int(input("Enter the third Number: "))
-print("The largest of the three numbers = ", max(a,b,c))
